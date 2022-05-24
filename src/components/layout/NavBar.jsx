97fff3f0aba07ca6cfa0f17b8c1c867cd998b6d0
Pepinosky <aastorgaestevan4@gmail.com>
2022-05-23 23:16:15 -0300

    small adjustment on navbar

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -5,6 +5,7 @@ const NavBar = () => {
     
     let Links = [
         {name: "Inicio", link:"/"},
+        {name: "Reservar", link:"/"},
         {name: "Cancelar", link:"/"},
         {name: "Estado de reserva", link:"/"},
         
@@ -17,7 +18,7 @@ const NavBar = () => {
         <div className='md:flex shadow-md items-center justify-between bg-white py-4 md:px-10 px-7  ' >
             
             <div className="text-lime-700 text-2xl pb-5 cursor-pointer flex items-center font-Antic">
-             <span className="flex mr-16 mb-0 pt-3 pb-0 "> 
+             <span className="flex mr-16 mb-0 pt-0 pb-0 "> 
                  <img className="h-20" src={LogoSpa} alt="logo" />            
              </span>
            
@@ -25,11 +26,11 @@ const NavBar = () => {
             <div onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden'>
           <ion-icon name={open ? 'close':'menu'}></ion-icon>
             </div>
-            <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ':'top-[-490px]'}`}>
+            <ul className={`md:flex md:items-center md:pb-0 md:pt-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 pt-12  transition-all duration-500 ease-in ${open ? 'top-20 ':'top-[-490px]'}`}>
                 {
                    Links.map((link)=>(
                        <li key={link.name} className='md:ml-8 text-xl md:my-0 my-7 '>
-                           <a href={link.link} className='text-lime-700 hover:bg-lime-900 hover:text-white hover:p-4 duration-500  ml-20'>{link.name}</a>
+                           <a href={link.link} className='text-lime-700 hover:bg-lime-900 hover:text-white hover:p-4 hover:rounded-md duration-500  ml-20'>{link.name}</a>
                        </li>
                    ) ) 
                 }
--- src/components/pages/User/reservation.jsx
+++ src/components/pages/User/reservation.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 
 const Reservas = () => {
-  let Services = [
+  let Services = 
+  [
     {id: "1", nombre:"servicio1"},
     {id: "2", nombre:"servicio2"},
     {id: "3", nombre:"servicio3"}
-    
-     ];
+  ];
 
     return (
     <div className='w-full h-full py-44 px-4'>
         <div className=' mx-auto  '>         
-          <div className='md:flex  flex-col items-center content-center justify-center p-1'>
+          <div className='md:flex  flex-col items-center content-center justify-center p-1 text-center sm:text-left'>
             <h1 className=' md:text-4xl sm:text-3xl text-2xl font-bold  '>RESERVA DE HORAS</h1>
             <h6 className='md:text-3xl sm:text-2xl font-light '>Selecciona segun tus preferencias</h6>
                   <form className='flex flex-col items-center'>
