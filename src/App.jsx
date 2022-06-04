4f0a1849692afe598dda4128916d01509e7866df
Pepinosky <aastorgaestevan4@gmail.com>
2022-06-03 23:04:16 -0300

    tested, todo: headless ui implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
 import React from 'react'
 import {Route, Routes } from "react-router-dom";
-import Reservation from './components/pages/User/reservation';
-import Cancellation  from './components/pages/User/cancellation';
-import Status from './components/pages/User/status';
-
+import Reservation from './components/pages/Reservation';
+import Cancellation  from './components/pages/Cancellation';
+import Status from './components/pages/Status';
+import { StateMachineProvider, createStore } from "little-state-machine";
 import NavBar from './components/layout/NavBar';
 
+createStore({});
 function App() {
   return (
     <>
+    <StateMachineProvider>
     <NavBar /> 
     
     <Routes>
-       <Route path="/" element={<Reservation />} />
+       <Route exact path="/" element={<Reservation />} />
        <Route path="/reservation" element={<Reservation />} />
       <Route path="/cancellation" element={<Cancellation />} />
       <Route path="/status" element={<Status />} />
+     
     
   </Routes>
      
-    
+  </StateMachineProvider>
     </>
    );
 }
